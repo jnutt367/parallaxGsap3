9e8ff7a0edcce8688617174997bb1305f3acff0d
Jason Nutt <jnutt367@gmail.com>
2021-04-29 14:53:35 -0400

    commit all dino days files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # parallaxGsap3
+Another Dino iteration
---
+++ app.js
@@ -0,0 +1,19 @@
+gsap.registerPlugin(ScrollTrigger)
+gsap.defaults({ ease: "none", duration: 1 })
+
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "wrapper",
+        start: "top 25%",
+        end: "+=100",
+        toggleActions: "restart none none reverse",
+        markers: true
+    }
+})
+
+.from(".wrapper", { y: 50 })
+    .from(".cavemen", { y: 150 }, 0)
+    .from("#text", { y: 250 }, 0)
+    .from("#tree", { y: 250 }, 0)
+    .from("#dino", { y: 500 }, 0)
